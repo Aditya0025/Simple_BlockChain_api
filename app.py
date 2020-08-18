33e3360ed438d89dfa55937b43aac4548ce9021c
ADITYA <aditya0025@yahoo.com>
2020-08-18 15:17:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,121 @@
+import uuid
+from flask import Flask, jsonify, request
+from BlockChain import Blockchain
+
+# Instantiate our Node
+
+app = Flask(__name__)
+
+# Generate a global unique address for this node
+node_identifier = str(uuid.uuid4()).replace('-','')
+
+# Instantiate the Blockchain
+
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    #  We run the proof og work algo to get the next proof
+    last_block = blockchain.last_block
+    last_proof= last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # we must get a reward after getting an proof
+    # he senderis "0" to signify that this node has mined a new coin.
+
+    blockchain.new_transcation(
+        sender= "0",
+        recipient = node_identifier,
+        amount=1, 
+    )
+
+    # Forge the new block by adding it to the chain
+    previous_hash= blockchain.hash(last_block)
+
+    block = blockchain.new_block(proof,previous_hash)
+
+    response = {
+        'message' : "New Block Forged",
+        'index' : block['index'],
+        'transcation' : block['transcations'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response),200
+
+@app.route('/transcation/new', methods= ['POST'])
+def new_transcation():
+    values= request.get_json()
+
+    # check that the required fields are in the POST'ed data
+
+    required= ['sender','recipient','amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # Create a new Transcation
+    index = blockchain.new_transcation(values['sender'],values['recipient'], values['amount'])
+
+    respose = {'message' : f'Transcation will be added to the block {index}'}
+
+    return jsonify(respose),201
+
+
+@app.route('/chain',methods = ['GET'])
+def full_path():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values= request.get_json()
+
+    nodes = values.get('nodes')
+
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response  = {
+        'message' : 'New nodes have been added',
+        'total_nodes' : list(blockchain.nodes)
+    }
+
+    return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods= ['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        respose: {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        respose = {
+            'message': 'Our chain is authorative',
+            'chain': blockchain.chain
+        }
+    return jsonify(respose),200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
 
